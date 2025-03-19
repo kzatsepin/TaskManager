354b7bc7b1f61ed762cf2271679b4e559ac1bb42
Kirill Zatsepin <kiryxa0011@gmail.com>
2025-03-19 17:39:35 +0900

    added all the directories

---
+++ internal/app/app.go
@@ -0,0 +1,4 @@
+package app
+
+// here all the dependencies must be included and the app should be started
+// this run() should be called from main()
---
+++ internal/config/config.go
@@ -0,0 +1,3 @@
+package config
+
+// here should be all the settings about databases, APIs and etc.
